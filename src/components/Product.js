cdf23237fce5088309cb58966e2ed5cd438bc5b0
Jacob Jenkins <me@jacobejenkins.com>
2017-12-10 16:42:57 -0500

    Fix variant selection

--- src/components/Product.js
+++ src/components/Product.js
@@ -36,7 +36,7 @@ class Product extends Component {
     let selectedOptions = this.state.selectedOptions
     selectedOptions[target.name] = target.value
 
-    const selectedVariant = Client.product.variantForOptions(
+    const selectedVariant = this.props.product.variantForOptions(
       this.props.product,
       selectedOptions
     )
