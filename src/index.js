372e81dced2df1875ef4026d5d2d4c7e6886a3c3
Jacob Jenkins <me@jacobejenkins.com>
2017-12-10 16:06:13 -0500

    fix Client intialization

--- src/index.js
+++ src/index.js
@@ -1,14 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
-import Client, { Config } from 'shopify-buy'
+import Client from 'shopify-buy'
 import './app.css'
 
-const config = new Config({
+export const client = new Client({
   storefrontAccessToken: '271343b2eefed9420df1c81558bb89be',
   domain: 'wipe-ya-docs-off-hell-store.myshopify.com',
 })
-
-export const client = new Client(config)
 
 ReactDOM.render(<App client={client} />, document.getElementById('root'))
